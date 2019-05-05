7b51dc44fcc766806e60dea5d22e36ff46ed0ab1
Sean <iamseanhong@gmail.com>
2019-05-05 11:56:23 -0700

    Style Banner Wrapper

--- src/components/common/Banner.js
+++ src/components/common/Banner.js
@@ -1,19 +1,60 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 import * as sh from '../../helpers/styleHelpers';
 
-const Banner = ({ title }) => {
+const Banner = ({ greeting, title, description }) => {
 	return (
 		<BannerWrapper>
-			<h1>{title}</h1>
+			<h1>
+				{greeting} <span className="title">{title}</span>
+			</h1>
+			<div className="info">
+				<p className="description">{description}</p>
+			</div>
 		</BannerWrapper>
 	);
 };
 
+// -----------------------------------------------------------------------------------------
+// ---------------------------------- Styled Components ------------------------------------
+// -----------------------------------------------------------------------------------------
 const BannerWrapper = styled.div`
-	padding: 100px;
-	border: 10px solid ${sh.colorOptions.primary};
+	/* setRen */
+	padding: 60px 32px;
+	text-align: center;
+	color: ${sh.colorOptions.white};
+	background: rgba(0, 0, 0, .7);
+	/* setLetterSpacing */
+	letter-spacing: 3px;
+
+	@media (min-width: 768px) {
+		width: 70vw;
+		border: 6px solid ${sh.colorOptions.primary};
+		p {
+			width: 75%;
+			margin: 0 auto;
+		}
+	}
+
+	p {
+		width: 85%;
+		margin: 0 auto;
+	}
+
+	h1 {
+		text-transform: capitalize;
+		color: ${sh.colorOptions.primary};
+		font-size: 48px;
+	}
+
+	.title {
+		color: ${sh.colorOptions.white};
+	}
+
+	.description {
+		color: red;
+	}
 `;
 
 export default Banner;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,12 +6,17 @@ import React from 'react';
 import ContainerHero from '../common/ContainerHero';
 import Banner from '../common/Banner';
 
-const Home = () => {
+const Home = ({ className }) => {
 	return (
-		<div>
+		<div className={className}>
 			<ContainerHero>
-				<Banner title="Hello World" />
+				<Banner
+					greeting="Welcome to"
+					title="Hello World"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias laudantium doloremque, quis maiores voluptatem, adipisci ipsam possimus sit in rerum aut dolore porro itaque error quo repellendus perspiciatis illum dolorum."
+				/>
 			</ContainerHero>
+			<div className="description">aaa</div>
 		</div>
 	);
 };
